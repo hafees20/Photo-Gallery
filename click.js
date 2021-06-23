98b5dc647934f521a07b1a0413702b156ee16109
TeQ BooK <kalipc74@gmail.com>
2021-06-23 21:24:50 +0530

    Modified

--- click.js
+++ click.js
@@ -1,10 +1,14 @@
 import { getImagedetails } from "./image.js";
-import { fetchImage } from "./image.js";
 import { createImage } from "./image.js";
+const btn1 = document.querySelector('#btn1');
 const btn2 = document.querySelector('#btn2');
 const nextImage = async () => {
     const imageDetails = await getImagedetails();
     createImage(imageDetails.url);
 }
-const imgFetching = await fetchImage();
+// const previousImage = async () => {
+//     const imageDetails = await getImagedetails();
+//     createImage(imageDetails.url);
+// }
 btn2.addEventListener('click', nextImage);
+// btn1.addEventListener('click', previousImage);
--- image.js
+++ image.js
@@ -2,35 +2,24 @@
 export async function getImagedetails() {
     let res;
     let randomId = Math.floor(Math.random() * 100);
+    if (randomId == 0) {
+        randomId = 1
+    }
+    const currentId = randomId;
     try {
-        res = await fetch(`https://jsonplaceholder.typicode.com/photos/${randomId}`);
+        res = await fetch(`https://jsonplaceholder.typicode.com/photos/${currentId}`);
         const imageDetails = await res.json();
-        console.log(imageDetails)
-        return imageDetails
+        console.log(imageDetails);
+        return imageDetails;
 
     }
     catch (error) {
         console.error(error);
     }
 }
-// Function to fetch image from url
-export async function fetchImage(imageUrl) {
-    try {
-        let res = await fetch(imageUrl)
-        const imageData = await res.json();
-        console.log(imageUrl)
-        return imageData;
-
-    }
-    catch (error) {
-        console.error(error)
-    }
-}
-
 export function createImage(imageUrl) {
-    const imageHTMLElement = document.createElement('img');
-    imageHTMLElement.id = 'loadedImage';
-
+    const imageHTMLElement = document.getElementById('pic');
+    const imgSpace = document.getElementById('imgSpace');
     imageHTMLElement.src = imageUrl;
-    document.body.appendChild(imageHTMLElement);
+    imgSpace.append(imageHTMLElement);
 }
